e04a871c08eabb66d52495b81cab9378f003fa50
Sasha Zezulinsky <alexander.zezulinsky@gmail.com>
2024-02-08 00:55:37 +0100

    fix cargo deny

--- shuttle/src/server/mod.rs
+++ shuttle/src/server/mod.rs
@@ -2,7 +2,7 @@
 use crate::EthApi;
 use futures::StreamExt;
 use handler::{HttpEthRpcHandler, PubSubEthRpcHandler};
-use shuttle_server::{ipc::IpcEndpoint, AnvilServer, ServerConfig};
+use shuttle_server::{ipc::IpcEndpoint, ShuttleServer, ServerConfig};
 use std::net::SocketAddr;
 use tokio::{io, task::JoinHandle};
 use tracing::trace;
@@ -12,7 +12,7 @@ mod handler;
 pub mod error;
 
 /// Configures an [axum::Server] that handles [EthApi] related JSON-RPC calls via HTTP and WS
-pub fn serve(addr: SocketAddr, api: EthApi, config: ServerConfig) -> AnvilServer {
+pub fn serve(addr: SocketAddr, api: EthApi, config: ServerConfig) -> ShuttleServer {
     let http = HttpEthRpcHandler::new(api.clone());
     let ws = PubSubEthRpcHandler::new(api);
     shuttle_server::serve_http_ws(addr, config, http, ws)
