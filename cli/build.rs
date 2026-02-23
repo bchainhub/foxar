83d749b4bec66c3351c23b218df213c2670894dc
Mykhailo Slyvka <misha.slyvka@wallit.eu>
2026-02-23 11:56:23 +0200

    fix: windows build

--- cli/build.rs
+++ cli/build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        println!("cargo:rustc-link-lib=advapi32");
+    }
     vergen::EmitBuilder::builder().build_timestamp().git_sha(true).emit().unwrap();
 }
